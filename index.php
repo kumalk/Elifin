4531f038208547eb178775e0a8e17fbb84d2a12a
Prashantha Kumanayake <p.kumanayake@gmail.com>
2025-02-25 13:46:30 +0200

    replaced the_content() with the_exceprt() and added the permalink to the tile with the_permalink()

--- index.php
+++ index.php
@@ -12,9 +12,9 @@ if (have_posts()) { //check is there any posts
     while (have_posts()) { //if availabel loop through them and load each one and ech content
         the_post(); ?>   
         <article>
-            <h2><?php the_title(); ?></h2>   <!-- load title of the post -->
+            <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>   <!-- load title of the post -->
             <p class="date-category"><?php echo get_the_date() ?> | <?php echo get_the_category_list(" ,") ?></p>  <!-- load and print date and category , we can use the_date() but some times issues so use this one. Also can use the_category() but the are load like point list lie by line, so use this get_category_list() and echo it to print them inline  -->
-            <p><?php the_content(); ?></p> <!-- load content of the post -->
+            <p><?php the_excerpt(); ?></p> <!-- load content of the post previously used :the_content() that loads full contnet , but now use the_excerpt() to short part-->
         </article>
         <?php
     }
